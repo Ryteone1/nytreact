cea908f78ebde1c27f1c0f474245ecddb823e52a
ryteone1 <rynlacy@yahoo.com>
2017-12-19 08:05:32 -0500

    basic jumbotron displaying

---
+++ create-react-express/client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+  <div className="jumbotron text-center">
+    <h1>New York Times React Scraper</h1>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="http://www.recipepuppy.com/about/api/"
+    >
+      Powered by React
+    </a>
+  </div>
+);
+
+export default Jumbotron;
--- create-react-express/scripts/build.js
+++ create-react-express/scripts/build.js
@@ -1,3 +1,5 @@
 const args = ["run build"];
 const opts = { stdio: "inherit", cwd: "client", shell: true };
 require("child_process").spawn("npm", args, opts);
+
+// mongodb://heroku_f7tc4xrb:c6llvtogd9prujuj85nk5lub75@ds047692.mlab.com:47692/heroku_f7tc4xrb
